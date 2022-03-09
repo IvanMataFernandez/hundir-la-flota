edcdfd1ba530d6af1730811bc8229c01759d175f
ivanm <ivanm@DESKTOP-VM1KO5D.home>
2022-03-09 21:12:22 +0100

    Imagen

--- Proyect/src/pack/Main.java
+++
@@ -1,9 +0,0 @@
-package pack;
-
-public class Main {
-
-	public static void main (String args[]) {
-		System.out.print("Hello World!");
-		System.out.print("   . Ok bye");
-	}
-}
---
+++ Proyect/src/pack/Pantalla.java
@@ -0,0 +1,48 @@
+package pack;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class Pantalla extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Pantalla frame = new Pantalla();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Pantalla() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setIcon(new ImageIcon(".\\Images\\o.png"));
+		contentPane.add(lblNewLabel, BorderLayout.CENTER);
+	}
+
+}
